4fb78d681384fc21a2b6125766856cd58ed66087
alvarezdap <alvarezdap@pm.me>
2023-02-27 04:24:02 +0000

    carpeta static para archivos de la pagina

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
   return render_template("home.html")
-
 
 if __name__ == "__main__":
   print("I'm inside the if now")
